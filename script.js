861167829e2e4c5545dbca55461ccaaac74ca5d7
Aisha Fathihah <aishafaathi@Mac.lan>
2026-08-03 00:35:04 +0530

    Describe your changes

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navMenu = document.getElementById("nav-menu");
 menuToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
+
+// Close menu when a link is clicked
+document.querySelectorAll("#nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+    });
+});
